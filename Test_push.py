0bbef006123d96d871d4868bd8a252e4febbebe1
Mrudul Banka <bankamrudul@gmail.com>
2019-10-16 08:44:58 -0500

    Added user interest

--- Test_push.py
+++ Test_push.py
@@ -1,1 +1,6 @@
 print("this is a test push")
+vd = input("Would you like to create highlights for a football game?")
+if vd == "yes":
+    print("Try to follow the ipynb")
+else:
+    print("Thank you for visiting, Do check my other repos")
