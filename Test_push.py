05e3e92c38a82c42702f45734925978b1ef58ad3
Mrudul Banka <bankamrudul@gmail.com>
2019-10-16 23:23:56 -0500

    Added user interest 2

--- Test_push.py
+++ Test_push.py
@@ -3,5 +3,6 @@ vd = input("Would you like to create highlights for a football game?")
 if vd == "yes":
     print("Try to follow the ipynb")
     print("Try to follow the ipynb")
+    print("Try to follow the ipynb")
 else:
     print("Thank you for visiting, Do check my other repos")
